b019784f619b47bb9b9cd2f4269979c093d7fb5e
zulfauzi92 <64009520+zulfauzi92@users.noreply.github.com>
2021-02-23 20:31:45 +0700

    usercontroller, relasi migrasi, tabel user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function getAuthenticatedUser()
+    {
+
+    }
+
+    public function register(Request $request)
+    {
+
+    }
+
+    public function login(Request $request)
+    {
+
+    }
+
+    public function logout() 
+    {
+
+    }
+
+    public function update(Request $request)
+    {
+        
+    }
+
+}
